1d680f8570510df95a8284063183f3cc4b93f356
Elina013 <holmberg.lina@yahoo.se>
2023-11-23 13:11:05 +0100

    Update add_product.php

--- funktioner/add_product.php
+++ funktioner/add_product.php
@@ -1,33 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Hämta formulärdata från POST
-    $name = $_POST["name"];
-    $description = $_POST["description"];
-    $price = $_POST["price"];
-    $image = $_POST["image"];
-
-    // Anslut till din MySQL-databas
-    $conn = new mysqli("localhost", "användarnamn", "lösenord", "crud_app");
-
-    // Kontrollera anslutningen
-    if ($conn->connect_error) {
-        die("Anslutningen misslyckades: " . $conn->connect_error);
-    }
-
-    // Skapa en SQL-fråga för att lägga till produkten
-    $sql = "INSERT INTO products (name, description, price, image) VALUES ('$name', '$description', '$price', '$image')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Produkten har lagts till framgångsrikt.";
-    } else {
-        echo "Fel vid tillägg av produkt: " . $conn->error;
-    }
-
-    // Stäng anslutningen
-    $conn->close();
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -35,17 +5,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <h1>Lägg till produkt</h1>
-    <form method="post" action="add_product.php">
+    <form action="add_product.php" method="POST">
         <label for="name">Namn:</label>
         <input type="text" name="name" required><br>
         <label for="description">Beskrivning:</label>
-        <textarea name="description" rows="4" required></textarea><br>
+        <textarea name="description"></textarea><br>
         <label for="price">Pris:</label>
         <input type="number" name="price" step="0.01" required><br>
         <label for="image">Bild (URL):</label>
         <input type="text" name="image"><br>
         <input type="submit" value="Lägg till">
     </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Hämta och sanera användarens inmatning
+        $name = $_POST["name"];
+        $description = $_POST["description"];
+        $price = $_POST["price"];
+        $image = $_POST["image"];
+
+        // Anslut till databasen och utför en INSERT-query för att lägga till produkten
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=crud_app", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $conn->prepare("INSERT INTO products (name, description, price, image) VALUES (:name, :description, :price, :image)");
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':price', $price);
+            $stmt->bindParam(':image', $image);
+
+            $stmt->execute();
+
+            echo "Produkten har lagts till framgångsrikt.";
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+    ?>
 </body>
 </html>
-
